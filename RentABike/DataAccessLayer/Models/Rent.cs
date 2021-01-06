684e0ae7d351e3b6c27f32713e1f4b937ceb2ec6
Emilija Dincev <ema.dincev@gmail.com>
2021-01-06 14:26:07 +0100

    Komit 7 - Kreirana klasa Rent u DataAccessLayer-u

---
+++ RentABike/DataAccessLayer/Models/Rent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class Rent
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public int ProductID { get; set; }
+        public decimal Rental_price { get; set; }
+        public DateTime Rental_date { get; set; }
+        public DateTime Rental_time { get; set; }
+    }
+}
