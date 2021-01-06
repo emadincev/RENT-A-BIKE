75b5c6bba7f3e8206d240b6ba0292e64e29ebe99
Emilija Dincev <ema.dincev@gmail.com>
2021-01-06 13:46:05 +0100

    Komit 1 - Kreirana baza RentDB i napravljena tabela Users.dbo

---
+++ dbo.Users.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[UserID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(20) NOT NULL, 
+    [Surname] NVARCHAR(30) NOT NULL, 
+    [Address] NVARCHAR(50) NOT NULL, 
+    [Telephone] NCHAR(15) NOT NULL, 
+    [Email] NVARCHAR(50) NULL, 
+    [Password] NVARCHAR(30) NULL
+)
