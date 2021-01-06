c2b7d0e691cba3543d3997fbb1cb4d8c304a9aca
Emilija Dincev <ema.dincev@gmail.com>
2021-01-06 14:31:19 +0100

    Komit 8 - Kreirana klasa RentPrice u DataAccessLayer-u

---
+++ RentABike/DataAccessLayer/Models/RentPrice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class RentPrice
+    {
+        public double Price { get; set; }
+        public string Hours { get; set; }
+        public string Bicycle { get; set; }
+        public string Scooter { get; set; }
+
+        public RentPrice(double price, string hours, string bicycle, string scooter)
+        {
+            Price = price;
+            Hours = hours;
+            Bicycle = bicycle;
+            Scooter = scooter;
+        }
+    }
+}
