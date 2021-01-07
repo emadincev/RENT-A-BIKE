8a49ad15e10e1e59105261a5e0a73c8903769aa4
Tanja Prodanovic <pttanja@gmail.com>
2021-01-07 09:19:38 +0100

    Kreirane metode u klasi RentBusiness

--- RentABike/BusinessLayer/RentBusiness.cs
+++ RentABike/BusinessLayer/RentBusiness.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataAccessLayer;
+using DataAccessLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,50 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer
 {
-    public class Class1
+    public class RentBusiness
     {
+        private readonly RentRepository rentRepository;
+
+        public RentBusiness()
+        {
+            this.rentRepository = new RentRepository();
+        }
+        public List<User> GetAllUsers()
+        {
+            return this.rentRepository.GetAllUsers();
+        }
+        public List<Product> GetAllProducts()
+        {
+            return this.rentRepository.GetAllProducts();
+        }
+        public bool InsertProduct (Product p)
+        {
+            if (this.rentRepository.InserProduct(p) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool InserUser(User u)
+        {
+            if (this.rentRepository.InserUser(u) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool CheckUser(User u)
+        {
+            List<User> userList = rentRepository.GetAllUsers();
+            foreach(User user in userList)
+            {
+                if(user.Password==u.Password && user.Email == u.Email)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
     }
 }
