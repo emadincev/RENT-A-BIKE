b5d44c993ad521665f002f4736bdeec021f20329
Miloš Dinčev <milosdincev98@gmail.com>
2021-01-08 14:13:45 +0100

    Kreirana SumOfInformations forma

---
+++ RentABike/RentABike/SumOfInformations.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace RentABike
+{
+    partial class SumOfInformations
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelInformation = new System.Windows.Forms.Label();
+            this.labelDateRent = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.labelSelectedTime = new System.Windows.Forms.Label();
+            this.labelTypeOfProduct = new System.Windows.Forms.Label();
+            this.labelType = new System.Windows.Forms.Label();
+            this.labelTotalPrice = new System.Windows.Forms.Label();
+            this.labelTotal = new System.Windows.Forms.Label();
+            this.buttonRent = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelInformation
+            // 
+            this.labelInformation.AutoSize = true;
+            this.labelInformation.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelInformation.Location = new System.Drawing.Point(257, 68);
+            this.labelInformation.Name = "labelInformation";
+            this.labelInformation.Size = new System.Drawing.Size(306, 25);
+            this.labelInformation.TabIndex = 0;
+            this.labelInformation.Text = "INFORMATION ABOUT RENT";
+            // 
+            // labelDateRent
+            // 
+            this.labelDateRent.AutoSize = true;
+            this.labelDateRent.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDateRent.Location = new System.Drawing.Point(137, 141);
+            this.labelDateRent.Name = "labelDateRent";
+            this.labelDateRent.Size = new System.Drawing.Size(94, 20);
+            this.labelDateRent.TabIndex = 1;
+            this.labelDateRent.Text = "Date Rent";
+            // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDate.Location = new System.Drawing.Point(365, 141);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(0, 20);
+            this.labelDate.TabIndex = 2;
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTime.Location = new System.Drawing.Point(137, 197);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(50, 20);
+            this.labelTime.TabIndex = 3;
+            this.labelTime.Text = "Time";
+            // 
+            // labelSelectedTime
+            // 
+            this.labelSelectedTime.AutoSize = true;
+            this.labelSelectedTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSelectedTime.Location = new System.Drawing.Point(365, 197);
+            this.labelSelectedTime.Name = "labelSelectedTime";
+            this.labelSelectedTime.Size = new System.Drawing.Size(0, 20);
+            this.labelSelectedTime.TabIndex = 4;
+            // 
+            // labelTypeOfProduct
+            // 
+            this.labelTypeOfProduct.AutoSize = true;
+            this.labelTypeOfProduct.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTypeOfProduct.Location = new System.Drawing.Point(137, 247);
+            this.labelTypeOfProduct.Name = "labelTypeOfProduct";
+            this.labelTypeOfProduct.Size = new System.Drawing.Size(146, 20);
+            this.labelTypeOfProduct.TabIndex = 5;
+            this.labelTypeOfProduct.Text = "Type Of Product";
+            // 
+            // labelType
+            // 
+            this.labelType.AutoSize = true;
+            this.labelType.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelType.Location = new System.Drawing.Point(365, 247);
+            this.labelType.Name = "labelType";
+            this.labelType.Size = new System.Drawing.Size(0, 20);
+            this.labelType.TabIndex = 6;
+            // 
+            // labelTotalPrice
+            // 
+            this.labelTotalPrice.AutoSize = true;
+            this.labelTotalPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTotalPrice.Location = new System.Drawing.Point(137, 303);
+            this.labelTotalPrice.Name = "labelTotalPrice";
+            this.labelTotalPrice.Size = new System.Drawing.Size(101, 20);
+            this.labelTotalPrice.TabIndex = 7;
+            this.labelTotalPrice.Text = "Total Price";
+            // 
+            // labelTotal
+            // 
+            this.labelTotal.AutoSize = true;
+            this.labelTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTotal.Location = new System.Drawing.Point(365, 303);
+            this.labelTotal.Name = "labelTotal";
+            this.labelTotal.Size = new System.Drawing.Size(0, 20);
+            this.labelTotal.TabIndex = 8;
+            // 
+            // buttonRent
+            // 
+            this.buttonRent.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRent.Location = new System.Drawing.Point(324, 362);
+            this.buttonRent.Name = "buttonRent";
+            this.buttonRent.Size = new System.Drawing.Size(133, 46);
+            this.buttonRent.TabIndex = 9;
+            this.buttonRent.Text = "Rent";
+            this.buttonRent.UseVisualStyleBackColor = true;
+            this.buttonRent.Click += new System.EventHandler(this.buttonRent_Click);
+            // 
+            // SumOfInformations
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonRent);
+            this.Controls.Add(this.labelTotal);
+            this.Controls.Add(this.labelTotalPrice);
+            this.Controls.Add(this.labelType);
+            this.Controls.Add(this.labelTypeOfProduct);
+            this.Controls.Add(this.labelSelectedTime);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.labelDateRent);
+            this.Controls.Add(this.labelInformation);
+            this.Name = "SumOfInformations";
+            this.Text = "SumOfInformations";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelInformation;
+        private System.Windows.Forms.Label labelDateRent;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Label labelSelectedTime;
+        private System.Windows.Forms.Label labelTypeOfProduct;
+        private System.Windows.Forms.Label labelType;
+        private System.Windows.Forms.Label labelTotalPrice;
+        private System.Windows.Forms.Label labelTotal;
+        private System.Windows.Forms.Button buttonRent;
+    }
+}
