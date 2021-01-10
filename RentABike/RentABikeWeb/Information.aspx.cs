843f1412bfc9bccd0d05374e073dc538a39e9c22
Lazar Starčević <lazarstarco@gmail.com>
2021-01-10 20:46:48 +0100

    Information page created

---
+++ RentABike/RentABikeWeb/Information.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RentABikeWeb
+{
+    public partial class Information : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            date.InnerText = DateTime.Now.ToLongDateString();
+            time.InnerText = DateTime.Now.ToShortTimeString();
+            type.InnerText = "Omae wa mou shindeiru";
+            price.InnerText = "NANI";
+        }
+
+        protected void rent_ServerClick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
